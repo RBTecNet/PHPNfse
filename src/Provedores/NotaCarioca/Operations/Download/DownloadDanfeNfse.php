886588e31ac2aa0264ec049cef8b0f0bdc6f98f6
RBTecNet <bruno@rbtecnologia.net.br>
2023-04-29 23:23:55 -0300

    nfsephp

--- src/Provedores/NotaCarioca/Operations/Download/DownloadDanfeNfse.php
+++ src/Provedores/NotaCarioca/Operations/Download/DownloadDanfeNfse.php
@@ -4,7 +4,13 @@ namespace Rbtecnet\Phpnfse\Provedores\NotaCarioca\Operations\Download;
 
 class DownloadDanfeNfse
 {
-    function DownloadDanfeNfse(){
-        return "Função de Download do Danfe da NotaCarioca";
+    function DownloadDanfeNfse($ambiente='homologacao', $inscricaomunicipal='', $notafiscal='', $codver=''){
+        $codver = str_replace('-','',$codver);
+        if ($ambiente=='homologacao'){
+            $baseurl="https://notacariocahom.rio.gov.br/contribuinte/notaprint.aspx?inscricao=$inscricaomunicipal&nf=$notafiscal&verificacao=$codver";
+        }else{
+            $baseurl="https://notacarioca.rio.gov.br/contribuinte/notaprint.aspx?inscricao=$inscricaomunicipal&nf=$notafiscal&verificacao=$codver";
+        }
+        return $baseurl;
     }
 }
